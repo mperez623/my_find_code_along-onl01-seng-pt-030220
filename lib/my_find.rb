e1c24818f17f038a6e08c128890a8de5734d7962
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-15 18:36:35 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,14 @@
 require 'pry'
 
 def my_find(collection)
+  i = 0 
+  while i < collection.length
+  res = yield(collection[i])
+  if res 
+    return collection[i]
+  end
+  i += 1
+  
+end
 
 end
